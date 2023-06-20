f78992fe85467df8a881c5087ff8633e7aa1e3d0
Nicolas Mayorga <namayorgav@gmail.com>
2023-06-19 22:34:42 -0500

    Fetching Data now is done in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
+import { useEffect, useState } from "react"
 import Characters from "./Characters"
 import Hero from "./Hero"
 import title from "./assets/title.png"
+import Loading from "./Loading"
 
 function App() {
+  const API_URL = "https://rickndmortyapi.com/api/character"
+
+  const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const fetchData = async (URL) => {
+    try {
+      const response = await fetch(URL)
+      const result = await response.json()
+      console.log(result.results)
+      setData(result.results)
+      setIsLoading(false)
+    } catch (error) {
+      setData([])
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData(API_URL)
+  }, [])
+
+  if (isLoading) {
+    return <Loading />
+  }
+
   return (
     <>
-      <main className="principal">
+      <section className="principal">
         <header>
           <img src={title} alt="" />
         </header>
         <Hero />
-      </main>
-      <Characters />
+      </section>
+      <Characters data={data} />
     </>
   )
 }
