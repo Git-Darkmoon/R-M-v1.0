70bac1345ba7d0db87e62a99aaab7009f7b87d78
Nicolas Mayorga <namayorgav@gmail.com>
2023-06-21 21:42:28 -0500

    feat: Use of the globalContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import Characters from "./Characters"
 import Hero from "./Hero"
 import Nav from "./Nav"
+import { useGlobalContext } from "./Context"
 
 function App() {
   const API_URL = "https://rickandmortyapi.com/api/character"
 
-  const [data, setData] = useState([])
-
-  const fetchData = async (URL) => {
-    try {
-      const response = await fetch(URL)
-      const result = await response.json()
-      console.log(result.results)
-      setData(result.results)
-    } catch (error) {
-      setData([])
-      console.log(error)
-    }
-  }
+  const { data, fetchData } = useGlobalContext()
 
   useEffect(() => {
     fetchData(API_URL)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
--- src/Characters.jsx
+++ src/Characters.jsx
@@ -1,24 +1,41 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react"
+import { SlMagnifier } from "react-icons/sl"
 import Character from "./Character"
+import { useGlobalContext } from "./Context"
 
-function Characters({ data }) {
+function Characters() {
+  const [characterName, setCharacterName] = useState("")
+
+  const { data, setData, filterCharacters } = useGlobalContext()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newItems = filterCharacters(data, characterName)
+    // console.log(newItems)
+    setData(newItems)
+  }
+
   return (
     <>
       <h2 className="characters-title">Characters</h2>
-      <input type="text" name="" autoComplete="off" />
+      <form className="characters-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="form-searchBar"
+          autoComplete="off"
+          placeholder="Search Characters"
+          value={characterName}
+          onChange={(e) => setCharacterName(e.target.value)}
+          required
+        />
+        <button id="form-searchBtn">
+          <SlMagnifier />
+        </button>
+      </form>
       <section className="characters-container">
         {data.map((item) => {
-          const { id, image, name, status, species } = item
-
-          return (
-            <Character
-              key={id}
-              image={image}
-              name={name}
-              status={status}
-              species={species}
-            />
-          )
+          return <Character key={item.id} {...item} />
         })}
       </section>
     </>
